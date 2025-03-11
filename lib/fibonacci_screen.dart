4f68601a671a7cfd8ab65a420518a631f4f7128b
Chitsanucha Jumrernsombut <c.jumrernsombut@accenture.com>
2025-03-11 23:46:55 +0700

    Create view model and data stream

--- lib/fibonacci_screen.dart
+++ lib/fibonacci_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:seven_solutions_mobile_assignment/constant/pattern_symbol.dart';
+import 'package:seven_solutions_mobile_assignment/fibonacci_view_model.dart';
 import 'package:seven_solutions_mobile_assignment/model/fibonacci_item.dart';
 
 class FibonacciListScreen extends StatefulWidget {
@@ -10,27 +11,24 @@ class FibonacciListScreen extends StatefulWidget {
 }
 
 class _FibonacciListScreenState extends State<FibonacciListScreen> {
-  final List<FibonacciItem> _fibonacciList = <FibonacciItem>[];
-
-  final List<FibonacciItem> _circleList = <FibonacciItem>[];
-  final List<FibonacciItem> _squareList = <FibonacciItem>[];
-  final List<FibonacciItem> _crossList = <FibonacciItem>[];
-
-  final ScrollController _scrollController = ScrollController();
+  late final FibonacciViewModel _viewModel;
+  late final ScrollController _scrollController;
 
   void _onScroll() {
     if (_scrollController.position.pixels ==
         _scrollController.position.maxScrollExtent) {
-      _generateFibonacci(20); // Load 20 more when scrolled to the bottom
+      _viewModel.generateFibonacci(20); // generate more Fibonacci numbers
     }
   }
 
   @override
   void initState() {
     super.initState();
+    _viewModel = FibonacciViewModel();
+    _scrollController = ScrollController();
 
     _scrollController.addListener(_onScroll);
-    _generateFibonacci(40); // Generate first 40 Fibonacci numbers
+    _viewModel.init();
   }
 
   @override
@@ -38,42 +36,8 @@ class _FibonacciListScreenState extends State<FibonacciListScreen> {
     _scrollController.removeListener(_onScroll);
     _scrollController.dispose();
 
+    _viewModel.dispose();
     super.dispose();
-  }
-
-  void _generateFibonacci(int count) {
-    setState(() {
-      int startIndex = _fibonacciList.length;
-      for (int i = 0; i < count; i++) {
-        int index = startIndex + i;
-        BigInt value;
-
-        if (index == 0) {
-          value = BigInt.zero;
-        } else if (index == 1) {
-          value = BigInt.one;
-        } else {
-          value =
-              _fibonacciList[index - 1].value + _fibonacciList[index - 2].value;
-        }
-
-        // Attach the symbol directly
-        PatternSymbol symbol = _getSymbolByIndex(index);
-
-        _fibonacciList.add(
-          FibonacciItem(index, value, symbol),
-        );
-      }
-    });
-  }
-
-  PatternSymbol _getSymbolByIndex(int index) {
-    int patternIndex = (index ~/ 4) % 2; // Switch between pattern A and B
-    int symbolIndex = index % 4; // Get symbol within the pattern
-
-    // debugPrint("@@@ Index $index => $patternIndex, $symbolIndex");
-
-    return PatternSet.patterns[patternIndex][symbolIndex];
   }
 
   @override
@@ -87,133 +51,136 @@ class _FibonacciListScreenState extends State<FibonacciListScreen> {
         ),
         centerTitle: true,
       ),
-      body: ListView.separated(
-        controller: _scrollController,
-        itemCount: _fibonacciList.length,
-        separatorBuilder: (context, index) => const Divider(
-          height: 0,
-          indent: 14,
-          endIndent: 14,
-        ),
-        itemBuilder: (context, index) {
-          final FibonacciItem item = _fibonacciList[index];
+      body: StreamBuilder<List<FibonacciItem>>(
+        stream: _viewModel.fibonacciListStream,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          } else {
+            final List<FibonacciItem> fibonacciList = snapshot.data!;
 
-          return ListTile(
-            onTap: () {
-              showModalListFilterBySymbol(item.symbol);
-            },
-            contentPadding: const EdgeInsets.symmetric(
-              vertical: 3,
-              horizontal: 16,
-            ),
-            tileColor: Colors.white,
-            leading: Container(
-              padding: const EdgeInsets.all(2),
-              decoration: const BoxDecoration(
-                color: Colors.blue,
-                shape: BoxShape.circle,
+            return ListView.separated(
+              controller: _scrollController,
+              itemCount: fibonacciList.length,
+              separatorBuilder: (context, index) => const Divider(
+                height: 0,
+                indent: 14,
+                endIndent: 14,
               ),
-              child: CircleAvatar(
-                radius: 16,
-                backgroundColor: Colors.white,
-                child: Text('${item.id + 1}'),
-              ),
-            ),
-            title: Text('Number: ${item.value}'),
-            trailing: Icon(
-              PatternSet.getSymbol(item.symbol),
-              color: Colors.blue[800],
-            ),
-          );
+              itemBuilder: (context, index) {
+                final FibonacciItem item = fibonacciList[index];
+
+                return ListTile(
+                  onTap: () {
+                    // showModalListFilterBySymbol(item.symbol);
+                  },
+                  contentPadding: const EdgeInsets.symmetric(
+                    vertical: 3,
+                    horizontal: 16,
+                  ),
+                  tileColor: Colors.white,
+                  leading: Container(
+                    padding: const EdgeInsets.all(2),
+                    decoration: const BoxDecoration(
+                      color: Colors.blue,
+                      shape: BoxShape.circle,
+                    ),
+                    child: CircleAvatar(
+                      radius: 16,
+                      backgroundColor: Colors.white,
+                      child: Text('${item.id + 1}'),
+                    ),
+                  ),
+                  title: Text('Number: ${item.value}'),
+                  trailing: Icon(
+                    PatternSet.getSymbol(item.symbol),
+                    color: Colors.blue[800],
+                  ),
+                );
+              },
+            );
+          }
         },
       ),
     );
   }
 
-  void addItemToList(FibonacciItem item) {
-    switch (item.symbol) {
-      case PatternSymbol.circle:
-        return _circleList.add(item);
-      case PatternSymbol.square:
-        return _squareList.add(item);
-      case PatternSymbol.cross:
-        return _crossList.add(item);
-      default:
-        debugPrint("Something went wrong !!!");
-        break;
-    }
-  }
+  // List<FibonacciItem>? getFibonacciItemListBySymbol(PatternSymbol symbol) {
+  //   switch (symbol) {
+  //     case PatternSymbol.circle:
+  //       return _circleList;
+  //     case PatternSymbol.square:
+  //       return _squareList;
+  //     case PatternSymbol.cross:
+  //       return _crossList;
+  //     default:
+  //       return null;
+  //   }
+  // }
 
-  List<FibonacciItem>? getFibonacciItemListBySymbol(PatternSymbol symbol) {
-    switch (symbol) {
-      case PatternSymbol.circle:
-        return _circleList;
-      case PatternSymbol.square:
-        return _squareList;
-      case PatternSymbol.cross:
-        return _crossList;
-      default:
-        return null;
-    }
-  }
+  // void showModalListFilterBySymbol(PatternSymbol symbol) {
+  //   List<FibonacciItem>? fiboList = getFibonacciItemListBySymbol(symbol);
 
-  void showModalListFilterBySymbol(PatternSymbol symbol) {
-    List<FibonacciItem>? fiboList = getFibonacciItemListBySymbol(symbol);
+  //   if (fiboList == null) return;
 
-    if (fiboList == null) return;
+  //   showModalBottomSheet<void>(
+  //     context: context,
+  //     builder: (BuildContext context) {
+  //       return Container(
+  //         height: 200,
+  //         decoration: const BoxDecoration(
+  //           color: Colors.amber,
+  //           borderRadius: BorderRadius.vertical(
+  //             top: Radius.circular(15),
+  //           ),
+  //         ),
+  //         child: ListView.separated(
+  //           itemCount: fiboList.length,
+  //           separatorBuilder: (context, index) => const Divider(
+  //             height: 0,
+  //             indent: 14,
+  //             endIndent: 14,
+  //           ),
+  //           itemBuilder: (context, index) {
+  //             final FibonacciItem item = fiboList[index];
 
-    showModalBottomSheet<void>(
-      context: context,
-      builder: (BuildContext context) {
-        return Container(
-          height: 200,
-          decoration: const BoxDecoration(
-            color: Colors.amber,
-            borderRadius: BorderRadius.vertical(
-              top: Radius.circular(15),
-            ),
-          ),
-          child: ListView.separated(
-            itemCount: fiboList.length,
-            separatorBuilder: (context, index) => const Divider(
-              height: 0,
-              indent: 14,
-              endIndent: 14,
-            ),
-            itemBuilder: (context, index) {
-              final FibonacciItem item = fiboList[index];
-
-              return ListTile(
-                onTap: () {
-                  debugPrint("Remove idx: $index => ${item.value}");
-                },
-                contentPadding: const EdgeInsets.symmetric(
-                  vertical: 3,
-                  horizontal: 16,
-                ),
-                tileColor: Colors.white,
-                leading: Container(
-                  padding: const EdgeInsets.all(2),
-                  decoration: const BoxDecoration(
-                    color: Colors.blue,
-                    shape: BoxShape.circle,
-                  ),
-                  child: CircleAvatar(
-                    radius: 16,
-                    backgroundColor: Colors.white,
-                    child: Text('${item.id + 1}'),
-                  ),
-                ),
-                title: Text('Number: ${item.value}'),
-                trailing: Icon(
-                  PatternSet.getSymbol(item.symbol),
-                  color: Colors.blue[800],
-                ),
-              );
-            },
-          ),
-        );
-      },
-    );
-  }
+  //             return ListTile(
+  //               onTap: () {
+  //                 debugPrint("Remove idx: $index => ${item.value}");
+  //               },
+  //               contentPadding: const EdgeInsets.symmetric(
+  //                 vertical: 3,
+  //                 horizontal: 16,
+  //               ),
+  //               tileColor: Colors.white,
+  //               leading: Container(
+  //                 padding: const EdgeInsets.all(2),
+  //                 decoration: const BoxDecoration(
+  //                   color: Colors.blue,
+  //                   shape: BoxShape.circle,
+  //                 ),
+  //                 child: CircleAvatar(
+  //                   radius: 16,
+  //                   backgroundColor: Colors.white,
+  //                   child: Text('${item.id + 1}'),
+  //                 ),
+  //               ),
+  //               title: Text('Number: ${item.value}'),
+  //               trailing: Icon(
+  //                 PatternSet.getSymbol(item.symbol),
+  //                 color: Colors.blue[800],
+  //               ),
+  //             );
+  //           },
+  //         ),
+  //       );
+  //     },
+  //   );
+  // }
 }
--- lib/fibonacci_view_model.dart
+++ lib/fibonacci_view_model.dart
@@ -1,1 +1,92 @@
-class FibonacciViewModel {}
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:seven_solutions_mobile_assignment/constant/pattern_symbol.dart';
+import 'package:seven_solutions_mobile_assignment/model/fibonacci_item.dart';
+
+class FibonacciViewModel {
+  final List<FibonacciItem> _fibonacciList = <FibonacciItem>[];
+  final List<FibonacciItem> _circleList = <FibonacciItem>[];
+  final List<FibonacciItem> _squareList = <FibonacciItem>[];
+  final List<FibonacciItem> _crossList = <FibonacciItem>[];
+
+  final _fibonacciListController = StreamController<List<FibonacciItem>>();
+  get fibonacciListStream => _fibonacciListController.stream;
+  get fibonacciList => _fibonacciList;
+
+  final _circleListController = StreamController<List<FibonacciItem>>();
+  get circleListStream => _circleListController.stream;
+  get circleList => _circleList;
+
+  final _squareListController = StreamController<List<FibonacciItem>>();
+  get squareListStream => _squareListController.stream;
+  get squareList => _squareList;
+
+  final _crossListController = StreamController<List<FibonacciItem>>();
+  get crossListStream => _crossListController.stream;
+  get crossList => _crossList;
+
+  init() {
+    generateFibonacci(40); // Generate first 40 Fibonacci numbers
+    _fibonacciListController.add(_fibonacciList);
+  }
+
+  dispose() {
+    _fibonacciListController.close();
+    _circleListController.close();
+    _squareListController.close();
+    _crossListController.close();
+  }
+
+  // increaseCounter() {
+  //   _counter += _counterRepository.getIncrement();
+  //   _counterController.sink.add(_counter);
+  // }
+
+  void addItemToList(FibonacciItem item) {
+    switch (item.symbol) {
+      case PatternSymbol.circle:
+        return _circleList.add(item);
+      case PatternSymbol.square:
+        return _squareList.add(item);
+      case PatternSymbol.cross:
+        return _crossList.add(item);
+      default:
+        debugPrint("Something went wrong !!!");
+        break;
+    }
+  }
+
+  void generateFibonacci(int count) {
+    int startIndex = _fibonacciList.length;
+    for (int i = 0; i < count; i++) {
+      int index = startIndex + i;
+      BigInt value;
+
+      if (index == 0) {
+        value = BigInt.zero;
+      } else if (index == 1) {
+        value = BigInt.one;
+      } else {
+        value =
+            _fibonacciList[index - 1].value + _fibonacciList[index - 2].value;
+      }
+
+      // Attach the symbol directly
+      PatternSymbol symbol = _getSymbolByIndex(index);
+
+      _fibonacciList.add(
+        FibonacciItem(index, value, symbol),
+      );
+    }
+  }
+
+  PatternSymbol _getSymbolByIndex(int index) {
+    int patternIndex = (index ~/ 4) % 2; // Switch between pattern A and B
+    int symbolIndex = index % 4; // Get symbol within the pattern
+
+    // debugPrint("@@@ Index $index => $patternIndex, $symbolIndex");
+
+    return PatternSet.patterns[patternIndex][symbolIndex];
+  }
+}
